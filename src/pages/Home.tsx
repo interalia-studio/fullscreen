18286e854dbfa10f3540fea449dc95ba8a9417c6
Vincent Ahrend <mail@vincentahrend.com>
2022-05-05 17:42:42 +0200

    Setup tauri

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,28 @@
-import React from "react";
+import React, { useMemo } from "react";
+import { v4 as uuid } from "uuid";
+import { Navigate } from "react-router-dom";
+
 import logoBMBF from "../assets/images/logo-bmbf.svg";
 import logoOKFN from "../assets/images/logo-okfn.svg";
 
-const Home = () => (
-  <main>
-    <h1>Fullscreen is a local-first collaborative whiteboard</h1>
-    <div className="logos">
-      <img
-        src={logoBMBF}
-        alt="gefördert vom Bundesministerium für Bildung und Forschung"
-      />
-      <img src={logoOKFN} alt="Logo Open Knowledge Foundation Deutschland" />
-    </div>
-  </main>
-);
+const Home = () => {
+  // Redirects to a random board when opening the native app
+  if ((window as any).__TAURI__) {
+    return <Navigate replace to={`/board/${uuid()}`} />;
+  }
+
+  return (
+    <main>
+      <h1>Fullscreen is a local-first collaborative whiteboard</h1>
+      <div className="logos">
+        <img
+          src={logoBMBF}
+          alt="gefördert vom Bundesministerium für Bildung und Forschung"
+        />
+        <img src={logoOKFN} alt="Logo Open Knowledge Foundation Deutschland" />
+      </div>
+    </main>
+  );
+};
 
 export default Home;
