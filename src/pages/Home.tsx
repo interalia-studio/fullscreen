f83c81d4db64c52d9c992f7a2c86876ec71170cc
bumbleblue <bumbleblue@users.noreply.github.com>
2022-07-22 18:44:05 +0200

    going with the flow

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,6 +5,8 @@ import { Navigate } from "react-router-dom";
 import { styled } from "../styles";
 import { isNativeApp } from "../lib/tauri";
 
+import HeroImage from "../assets/images/visual_play.png";
+
 import LogoBMBF from "../assets/images/logo-bmbf.svg";
 import LogoOKFN from "../assets/images/logo-okfn.svg";
 
@@ -16,25 +18,29 @@ export const Home = () => {
 
   return (
     <HomeContainer>
-      <Heading>Fullscreen is a local-first collaborative whiteboard</Heading>
-      <Feature>
-        <h2>No cloud required</h2>
-        <p>
-          Fullscreen is local-first software. Your data will live on your computer, giving you full control over sharing and deleting.  You can work offline as well as in real-time in a collaborative session.          
-        </p>
-      </Feature>
-      <Feature>
-        <h2>Open and secure</h2>
-        <p>
-          Fullscreen is open-source software. You will always be able to see what is in our codebase and have independent people check it for security. We implement strong privacy and safety standards.          
-        </p>
-      </Feature>
-      <Feature>
+      <Heading>Fullscreen</Heading>
+      <Subheading>&mdash;truly, yours</Subheading>
+      <HeroImage alt="Fullscreen is a visual canvas app" />
+      <Features>
+        <div>
+          <h2>No cloud required</h2>
+          <p>
+            Fullscreen is local-first software. Your data will live on your computer, giving you full control over sharing and deleting.  You can work offline as well as in real-time in a collaborative session.          
+          </p>
+        </div>
+        <div>
+          <h2>Open and secure</h2>
+          <p>
+            Fullscreen is open-source software. You will always be able to see what is in our codebase and have independent people check it for security. We implement strong privacy and safety standards.          
+          </p>
+        </div>
+        <div>
         <h2>A canvas for teams</h2>
         <p>
           Fullscreen focuses on visual note-taking for teams. This includes the basics for a whiteboard (texts, sticky notes, sections), as well as visualisation and facilitation features. Fullscreen is beginner-friendly in the web browser and offers expert options in the native app.         
         </p>
-      </Feature>
+        </div>
+      </Features>
       <Logos>
         <LogoBMBF alt="gefördert vom Bundesministerium für Bildung und Forschung" />
         <LogoOKFN alt="Logo Open Knowledge Foundation Deutschland" />
@@ -45,7 +51,7 @@ export const Home = () => {
 
 const HomeContainer = styled("main", {
   display: "flex",
-  alignItems: "center",
+  alignItems: "left",
   flexDirection: "column",
   padding: "$4",
   height: "100vh",
@@ -55,9 +61,9 @@ const HomeContainer = styled("main", {
 });
 
 const Heading = styled("h1", {
-  marginBottom: "3em",
+  marginBottom: "1em",
   maxWidth: "15em",
-  textAlign: "center",
+  textAlign: "left",
   fontFamily: "$text",
   fontSize: "3em",
   color: "$blue",
@@ -67,14 +73,27 @@ const Heading = styled("h1", {
   },
 });
 
-const Feature = styled("h2", {
+const Subheading = styled("h2", {
   marginBottom: "3em",
   maxWidth: "15em",
-  textAlign: "center",
+  textAlign: "right",
   fontFamily: "$text",
-  fontSize: "1em",  
+  fontSize: "1.8em",  
   color: "$blue",
   wordBreak: "keep-all",
+  "@md": {
+    fontSize: "3em",
+  },
+});
+
+const Features = styled("section", {
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "left",
+  "@sm": {
+    flexDirection: "row",
+    justifyContent: "center",
+  },
 });
 
 const Logos = styled("section", {
