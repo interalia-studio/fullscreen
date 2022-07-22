a6a7d58c517e66af8263e3795d8e4a738d28f0e3
bumbleblue <bumbleblue@users.noreply.github.com>
2022-07-22 17:53:00 +0200

    first attempt

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -17,6 +17,24 @@ export const Home = () => {
   return (
     <HomeContainer>
       <Heading>Fullscreen is a local-first collaborative whiteboard</Heading>
+      <Feature>
+        <h2>No cloud required</h2>
+        <p>
+          Fullscreen is local-first software. Your data will live on your computer, giving you full control over sharing and deleting.  You can work offline as well as in real-time in a collaborative session.          
+        </p>
+      </Feature>
+      <Feature>
+        <h2>Open and secure</h2>
+        <p>
+          Fullscreen is open-source software. You will always be able to see what is in our codebase and have independent people check it for security. We implement strong privacy and safety standards.          
+        </p>
+      </Feature>
+      <Feature>
+        <h2>A canvas for teams</h2>
+        <p>
+          Fullscreen focuses on visual note-taking for teams. This includes the basics for a whiteboard (texts, sticky notes, sections), as well as visualisation and facilitation features. Fullscreen is beginner-friendly in the web browser and offers expert options in the native app.         
+        </p>
+      </Feature>
       <Logos>
         <LogoBMBF alt="gefördert vom Bundesministerium für Bildung und Forschung" />
         <LogoOKFN alt="Logo Open Knowledge Foundation Deutschland" />
@@ -47,6 +65,16 @@ const Heading = styled("h1", {
   "@md": {
     fontSize: "5em",
   },
+});
+
+const Heading = styled("h2", {
+  marginBottom: "3em",
+  maxWidth: "15em",
+  textAlign: "center",
+  fontFamily: "$text",
+  fontSize: "1em",  
+  color: "$blue",
+  wordBreak: "keep-all",
 });
 
 const Logos = styled("section", {
